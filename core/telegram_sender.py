b79e068335d4abd5361b64b0eea2b46ae438634c
hellseye <brutalyso@gmail.com>
2026-02-17 13:02:12 +0300

    233232

--- core/telegram_sender.py
+++ core/telegram_sender.py
@@ -1,10 +1,18 @@
-from aiogram import Bot
-import asyncio
+import requests
 
 BOT_TOKEN = "8529435887:AAHjrDxKJ8CBBtagAWb4zZ7mtJaiEfTc5S0"
-
-bot = Bot(token=BOT_TOKEN)
+BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 
 async def send_message(tg_id: int, text: str):
-    await bot.send_message(tg_id, text)
+    try:
+        requests.post(
+            f"{BASE_URL}/sendMessage",
+            json={
+                "chat_id": tg_id,
+                "text": text
+            },
+            timeout=10
+        )
+    except Exception as e:
+        print("Telegram send error:", e)
